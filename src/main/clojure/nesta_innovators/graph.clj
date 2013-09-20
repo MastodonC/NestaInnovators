f056095a155bba9e04d78e291aa1cc00f3665351
Neale Swinnerton <neale@isismanor.com>
2013-09-20 05:33:43 +0100

    Snapshot.

--- src/main/clojure/nesta_innovators/graph.clj
+++ src/main/clojure/nesta_innovators/graph.clj
@@ -2,7 +2,9 @@
   "Store a graph"
   (:require [clojurewerkz.neocons.rest :as nr]
             [clojurewerkz.neocons.rest.nodes :as nn]
-            [clojurewerkz.neocons.rest.relationships :as nrl])
+            [clojurewerkz.neocons.rest.relationships :as nrl]
+            [clojurewerkz.neocons.rest.cypher :as cy]
+            [clojure.tools.logging :as log])
   (:import [nesta_innovators.impl.protocols Lifecycle]))
 
 (defrecord Neo4jStore [uri]
@@ -16,9 +18,11 @@
     (->Neo4jStore uri)))
 
 (defn add-user! [username attrs]
+  (log/infof "Adding User %s" username)
   (nn/create (merge {:user username} attrs)))
 
 (defn add-organization! [orgname attrs]
+  (log/infof "Adding Organization %s" orgname)
   (nn/create (merge {:org orgname} attrs)))
 
 (defn link-user-and-organization! [user-id org-id])
@@ -28,3 +32,6 @@
 (defn link-user-and-repo [user repo & [attrs]])
 
 (defn follows! [follower followee])
+
+(defn find-user [username]
+  (cy/tquery (format "START person=node:node_auto_index(user='%s') RETURN person" username)))
