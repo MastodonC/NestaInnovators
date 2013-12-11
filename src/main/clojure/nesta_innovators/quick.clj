24f2f4a730da161dceecd482ef84a44a9458b82b
Neale Swinnerton <neale@isismanor.com>
2013-12-11 11:17:25 +0000

    Tweaks to how data downloaded.

--- src/main/clojure/nesta_innovators/quick.clj
+++ src/main/clojure/nesta_innovators/quick.clj
@@ -1,15 +1,16 @@
 (ns nesta-innovators.quick
   "quick data downloader"
   (:gen-class)
-  (:require [org.httpkit.client :as http]
-            [clojure.core.async :refer [go chan <!! <! put! take! alt! timeout mult tap] :as a]
-            [clj-http.links :as http-links]
-            [clojure.data.csv :as csv]
-            [clojure.set :as set]
-            [cheshire.core :as json]
-            [clojure.java.io :as io]
+  (:require [clojure.tools.cli     :refer [cli]]
+            [org.httpkit.client    :as http]
+            [clojure.core.async    :refer [go chan <!! <! put! take! alt! timeout mult tap] :as a]
+            [clj-http.links        :as http-links]
+            [clojure.data.csv      :as csv]
+            [clojure.set           :as set]
+            [cheshire.core         :as json]
+            [clojure.java.io       :as io]
             [clojure.tools.logging :as log]
-            [clojure.string :as str]))
+            [clojure.string        :as str]))
 
 (def BASE_API_URI "https://api.github.com/")
 
@@ -24,13 +25,17 @@
           
 (defn now [] (System/currentTimeMillis))
 
+;; TODO move this into component setup.
+(def ^:dynamic *auth* (:thor2013  tokens))
+
 (defn http-get 
   ([url] (http-get nil url))
   ([opts url]
      (log/debugf "HTTP GET %s" url)
      (let [c (chan)]
        (http/get url
-                 (merge auth2 opts)
+                 (merge {:headers
+                         {"Authorization" (str "token " *auth*)}} opts)
                  (fn [r] (put! c r)))
        c)))
 
@@ -43,9 +48,9 @@
 
 (defn ->long [s]
   (condp #(%1 %2) s
-      nil? nil
-      number? (long s)
-      (Long/parseLong s)))
+    nil?    Long/MAX_VALUE
+    number? (long s)
+    (Long/parseLong s)))
 
 (def rate-limit (atom {}))
 
@@ -83,26 +88,70 @@
 (defn clean [x]
   (into {} (remove #(.endsWith (name (first %)) "_url") x)))
 
+(defn decode-link-headers [response]
+  (if-let [link-headers (get-in response [:headers :link])]
+    (let [link-headers (if (coll? link-headers)
+                         link-headers
+                         [link-headers])]
+      (assoc response
+        :links
+        (apply merge (for [link-header link-headers]
+                       (http-links/read-link-headers link-header)))))
+    response))
+
+(defn next-link-uri [r]
+  (-> r
+      decode-link-headers
+      (get-in [:links :next :href])))
+
+(defn- split-chan 
+  "Takes a count and a channel and returns a vector of n output channels.
+   A value is read from the in channels and copied to the output channels."
+  [n in]
+  (let [in' (chan)
+        m (mult in)
+        outs     (reduce (fn [a c] (conj a (tap m (chan)))) [] (range n))]
+    (go (>! in' (<! in)))
+    outs))
+
+(defn assoc-header [m k r]
+  (-> m
+      (get-in [:headers k])))
+
 (defn user-details 
   "Takes a github user login and returns a channel containing a map of
    the user details after removing some redundant information."
   [login]
-  (go (some->> (->uri "users" login)
-           request-and-process 
-           <!
-           :body
-           clean)))
+  (go 
+   (let [r (<! (request-and-process (->uri  "users" login)))]
+     (-> r
+         :body
+         clean
+         (assoc :etag (get-etag r))))))
 
 (defn user-followers
   "Takes a github user login and returns a channel containing a
    sequence of the login of each of those users following the given
    user."
-  [login]
-  (go (->> (->uri "users" login "followers")
-           request-and-process 
-           <!
-           :body
-           (map :login))))
+  ([login]
+     (user-followers login (->uri "users" login "followers")))
+  ([login uri]
+     (go
+      (let [r (<! (request-and-process uri))]
+        (vector 
+         (->> r
+              :body
+              (map :login))
+         (next-link-uri r))))))
+
+(defn all-user-followers
+  ([login] (all-user-followers login (->uri "users" login "followers")))
+  ([login uri]
+     (let [[page next-uri] (map <!! (user-followers login uri))]
+       (if next-uri
+         (lazy-cat page
+                   (all-user-followers login next-uri))
+         page))))
 
 (defn user-repos
   "Takes a github user login and returns a channel containing a map of
@@ -115,27 +164,13 @@
            (map clean)
            (map #(assoc % :owner login)))))
 
-(defn decode-link-headers [response]
-  (if-let [link-headers (get-in response [:headers :link])]
-    (let [link-headers (if (coll? link-headers)
-                         link-headers
-                         [link-headers])]
-      (assoc response
-        :links
-        (apply merge (for [link-header link-headers]
-                       (http-links/read-link-headers link-header)))))
-    response))
-
 (defn users
   "Returns a 2 element sequence with a channel containing a sequence of a page of user details and the next page data.
    Since argument can be passed to indicate the last seen id and
    retrieve the following page."
   ([] (users nil))
   ([next-uri]
-     (let [in (chan)
-           m1 (mult in)
-           out1 (tap m1 (chan))
-           out2 (tap m1 (chan))]
+     (let [[in out1 out2] (split-chan 2)]
        (go (>! in (<! (request-and-process (or next-uri (->uri "users"))))))
        (vector
         (go
@@ -145,10 +180,7 @@
               (keep (comp user-details :login))
               (map <!!) ;; TODO - does mapping this separately affect parallelism?           
               ))
-        (go (-> out2
-                 <!
-                 decode-link-headers
-                 (get-in [:links :next :href])))))))
+        (next-link-uri out2)))))
 
 (defn all-users 
   "Returns a lazy sequence of all users. Lazyness is per page." 
@@ -156,25 +188,25 @@
   ([uri]
      (log/debug uri)
      (let [[page next-uri] (map <!! (users uri))]
-       (lazy-cat page
-                 (all-users next-uri)))))
+       (if next-uri
+         (lazy-cat page
+                   (all-users next-uri))
+         page))))
 
 (defn clean-csv [xs]
-  (mapv #(if (string? %) (str/replace % #"[^\w\t \.@/:'\",!#\$%^&\*()_\+]" " ") %) xs))
+  (mapv #(if (string? %) (str/replace % #"[^\w\t \.@/:'\",!#\$%^&\*()_\+\-]" " ") %) xs))
 
-(defn dump-users-to-csv [users per-file dir filename-template]
-  (let [users (partition per-file users)
-        ->vals (juxt :id :login :followers :following :gravatar_id 
-                     :name :updated_at :bio :location :public_repos 
-                     :created_at :site_admin :url :email :type 
-                     :public_gists :hireable :blog  :company)]
-    (loop [n 0 [data & more] users]
+
+(defn dump-to-csv [coll per-file dir filename-template vals-fn]
+  (let [n (->long per-file)
+        coll (partition n n nil coll)]
+    (loop [n 0 [data & more] coll]
       (with-open [out (io/writer (io/file dir (format filename-template n)))]
-        (csv/write-csv out (mapv (comp clean-csv ->vals) data) :quote? (constantly true)))
+        (csv/write-csv out (mapv (comp clean-csv vals-fn) data) :separator \tab :quote? (constantly true)))
       (when more (recur (inc n) more)))))
 
 (defn rate-limit-stats []
-  (let [to-date #(java.util.Date. (* 1000 (Long/parseLong %)))]
+  (let [to-date #(java.util.Date. (* 1000 (->long %)))]
     (clojure.pprint/pprint 
      (-> (http-get "https://api.github.com/rate_limit")
          <!!
@@ -184,13 +216,54 @@
                        :x-ratelimit-limit])
          (update-in [:x-ratelimit-reset] to-date)))))
 
-(defn -main [& args]
-  (let [[dir per-file & more] args]
-    (let [since (first more)]
-      (dump-users-to-csv 
-       (all-users 
-        (str "https://api.github.com/users?since=" since)) 
-       (Long/parseLong per-file) 
-       dir
-       "users_%010d.tsv"))))
+(defn dump-users [per-file [dir & more]]
+  (let [n (->long per-file) 
+        since (first more)]
+    (dump-to-csv 
+     (all-users 
+      (str "https://api.github.com/users?since=" since)) 
+     (->long n) 
+     dir
+     "users_%010d.tsv"
+     (juxt :id :login :followers :following :gravatar_id 
+           :name :updated_at :bio :location :public_repos 
+           :created_at :site_admin :url :email :type 
+           :public_gists :hireable :blog  :company))))
 
+(defn dump-followers [per-file [user-list dir]]
+  (with-open [in (io/reader user-list)]
+    (doseq [login (line-seq in)]
+      (dump-to-csv
+       (map vector (repeat login) (all-user-followers login))
+       nil
+       dir
+       (str login "_followers.tsv")
+       identity))))
+
+(defn dump-repos [[user-list per-file]])
+
+(defn -main [& args]
+  (let [[opts args banner]
+        (cli args
+             ["-h" "--help" "Show help"
+              :flag true :default false]
+             ["-a" "--account" (str "Choose account, one of " (apply str (interpose \, (map name (keys tokens)))))]
+             ["-s" "--partition-size" "Partition Size"
+              :default 1000 :parse-fn ->long]
+             ["-u" "--users" "Download users"
+              :flag true :default false]
+             ["-f" "--followers" "Download followers"
+              :flag true :default false]
+             ["-r" "--repos" "Download repos"
+              :flag true :default false])]
+
+    (when (:help opts)
+      (println banner)
+      (System/exit 0))
+      
+    (let [per-file (:partition-size opts)]
+      (condp #(%1 %2) opts
+        :users     (dump-users per-file args)
+        :followers (dump-followers per-file args)
+        :repos     (dump-repos per-file args))))
+  )
